9e212b06f81d73b529d1c874d4c8aaa8c6f2d774
marcel <mbfodor@gmail.com>
2019-03-25 23:16:15 +0100

    Add support for new game button

--- src/GUI/GUIWindowMainMenu.h
+++ src/GUI/GUIWindowMainMenu.h
@@ -5,21 +5,30 @@
 #ifndef A_STRESSFUL_MACHINE_GUIWINDOWMAINMENU_H
 #define A_STRESSFUL_MACHINE_GUIWINDOWMAINMENU_H
 
+#include "GUIWindowGameplay.h"
+
 #include <QWidget>
 #include <QPushButton>
 #include <QGridLayout>
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QFont>
+#include <QInputDialog>
 
 // class QPushButton;
 class GUIWindowMainMenu : public QWidget
 {
     // Initialize macro for qt
     Q_OBJECT
+
 public:
     explicit GUIWindowMainMenu(QWidget *parent = nullptr);
     ~GUIWindowMainMenu();
+
+private slots:
+    void createNewGameDialog();
+    void createGameplayWindow();
+
 private:
     QGridLayout *windowLayout;
     QVBoxLayout *buttonLayout;
@@ -29,6 +38,7 @@ private:
     QPushButton *loadGameButton;
     QPushButton *settingsButton;
     QPushButton *quitGameButton;
+    QInputDialog *newGameDialog;
 };
 
 
