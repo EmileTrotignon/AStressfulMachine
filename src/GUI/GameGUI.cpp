1191b2b187f8efab15a156bfcd59d27f560ac9f3
marcel <mbfodor@gmail.com>
2019-04-09 11:33:56 +0200

    Add escape menu

--- src/GUI/GameGUI.cpp
+++ src/GUI/GameGUI.cpp
@@ -4,12 +4,10 @@
 
 #include "GameGUI.h"
 
-#include <iostream>
-
 GameGUI::GameGUI(const string &saves_dir_, const string &gamefiles_dir_) : QStackedWidget(nullptr),
                                                                            Game(saves_dir_, gamefiles_dir_)
 {
-    std::cout << "Constructing GameGUI" << std::endl;
+    qDebug() << "Constructing GameGUI";
     setObjectName("GUIWindow");
     setWindowTitle("A Stressful Machine");
 
@@ -24,6 +22,11 @@ GameGUI::GameGUI(const string &saves_dir_, const string &gamefiles_dir_) : QStac
     addWidget(sandbox);
 
     setCurrentWidget(main_menu_widget);
+
+    qDebug() << "Creating shortcut";
+    auto *shortcut = new QShortcut(this);
+    shortcut->setKey(Qt::Key_Escape);
+    connect(shortcut, SIGNAL (activated()), this, SLOT (open_esc_menu()));
 }
 
 
@@ -40,4 +43,45 @@ void GameGUI::open_adventure_mode()
 void GameGUI::open_sandbox()
 {
     setCurrentWidget(sandbox);
+}
+
+void GameGUI::open_esc_menu()
+{
+
+    qDebug() << "Esc shortcut pressed";
+
+    if (currentWidget() != main_menu_widget)
+    {
+        auto esc_dlg = new QDialog(currentWidget());
+        esc_dlg->setObjectName("Escape dialog");
+        esc_dlg->setWindowTitle("A Stressful Machine");
+        // esc_menu = new QDialogButtonBox(QDialogButtonBox::Close | QDialogButtonBox::Ok, Qt::Vertical, esc_dlg);
+        auto esc_dlg_resume_button = new QPushButton("Resume", esc_dlg);
+        auto esc_dlg_settings_button = new QPushButton("Settings", esc_dlg);
+        auto esc_dlg_quit_button = new QPushButton("Quit to main menu", esc_dlg);
+        auto esc_dlg_layout = new QVBoxLayout(esc_dlg);
+        esc_dlg_layout->addWidget(esc_dlg_resume_button);
+        esc_dlg_layout->addWidget(esc_dlg_settings_button);
+        esc_dlg_layout->addWidget(esc_dlg_quit_button);
+        esc_dlg->setModal(true);
+        esc_dlg->open();
+
+        // Connect resume button
+        connect(esc_dlg_resume_button, SIGNAL (clicked(bool)), this, SLOT (esc_dlg_rejected()));
+        connect(this, SIGNAL (resume_play(int)), esc_dlg, SLOT (done(int)));
+
+        // Connect quit button
+        connect(esc_dlg_quit_button, SIGNAL (clicked(bool)), this, SLOT (esc_dlg_quit()));
+    }
+}
+
+void GameGUI::esc_dlg_rejected()
+{
+    emit resume_play();
+}
+
+void GameGUI::esc_dlg_quit()
+{
+    setCurrentWidget(main_menu_widget);
+    emit resume_play();
 }
